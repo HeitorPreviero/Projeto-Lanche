0e0807789e4b4cdbdd9c0f3a1a53e8ead374fc16
HeitorPreviero <heitorpreviero@gmail.com>
2021-10-06 16:46:59 -0300

    first commit

---
+++ src/main/java/com/testefinch/finchlanche/FinchlancheApplication.java
@@ -0,0 +1,13 @@
+package com.testefinch.finchlanche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinchlancheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinchlancheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testefinch/finchlanche/FinchlancheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testefinch.finchlanche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinchlancheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
